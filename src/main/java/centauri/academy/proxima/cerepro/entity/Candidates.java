70de3f510c76ce75f40bf7668fc6f2a26a1c2d47
Antonio <Antonio@LAPTOP-GPM0CA74>
2023-02-01 16:27:20 +0100

    Candidates fields updated

--- src/main/java/centauri/academy/proxima/cerepro/entity/Candidates.java
+++ src/main/java/centauri/academy/proxima/cerepro/entity/Candidates.java
@@ -33,24 +33,6 @@ public class Candidates implements Serializable, EntityInterface{
 	
 	@Column(name="candidacy_date_time")
 	private Timestamp candidacy_date_time;
-
-	@Column(name="firstname")
-	private String firstname;
-	
-	@Column(name="lastname")
-	private String lastname;
-	
-	@Column(name="email")
-	private String email;
-	
-	@Column(name="regdate")
-	private Timestamp regdate;
-	
-	@Column(name="inserted_by")
-	private Long inserted_by;
-	
-	@Column(name="candidate_state_code")
-	private Long candidate_state_code;
 	
 	/**
 	 * 
@@ -59,19 +41,12 @@ public class Candidates implements Serializable, EntityInterface{
 		super();
 	}
 
-	public Candidates(Long id, Long user_id, String course_code, Timestamp candidacy_date_time, String firstname,
-			String lastname, String email, Timestamp regdate, Long inserted_by, Long candidate_state_code) {
+	public Candidates(Long id, Long user_id, String course_code, Timestamp candidacy_date_time) {
 		super();
 		this.id = id;
 		this.user_id = user_id;
 		this.course_code = course_code;
 		this.candidacy_date_time = candidacy_date_time;
-		this.firstname = firstname;
-		this.lastname = lastname;
-		this.email = email;
-		this.regdate = regdate;
-		this.inserted_by = inserted_by;
-		this.candidate_state_code = candidate_state_code;
 	}
 
 	/**
@@ -130,95 +105,10 @@ public class Candidates implements Serializable, EntityInterface{
 		this.candidacy_date_time = candidacy_date_time;
 	}
 
-	/**
-	 * @return the firstname
-	 */
-	public String getFirstname() {
-		return firstname;
-	}
-
-	/**
-	 * @param firstname the firstname to set
-	 */
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	/**
-	 * @return the lastname
-	 */
-	public String getLastname() {
-		return lastname;
-	}
-
-	/**
-	 * @param lastname the lastname to set
-	 */
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
-
-	/**
-	 * @param email the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	/**
-	 * @return the regdate
-	 */
-	public Timestamp getRegdate() {
-		return regdate;
-	}
-
-	/**
-	 * @param email the email to set
-	 */
-	public void setRegdate(Timestamp regdate) {
-		this.regdate = regdate;
-	}
-
-	/**
-	 * @return the inserted_by
-	 */
-	public Long getInserted_by() {
-		return inserted_by;
-	}
-
-	/**
-	 * @param inserted_by the inserted_by to set
-	 */
-	public void setInserted_by(Long inserted_by) {
-		this.inserted_by = inserted_by;
-	}
-
-	/**
-	 * @return the candidate_state_code
-	 */
-	public Long getCandidate_state_code() {
-		return candidate_state_code;
-	}
-
-	/**
-	 * @param candidate_state_code the candidate_state_code to set
-	 */
-	public void setCandidate_state_code(Long candidate_state_code) {
-		this.candidate_state_code = candidate_state_code;
-	}
-
 	@Override
 	public String toString() {
 		return "Candidates [id=" + id + ", user_id=" + user_id + ", course_code=" + course_code
-				+ ", candidacy_date_time=" + candidacy_date_time + ", firstname=" + firstname + ", lastname=" + lastname
-				+ ", email=" + email + ", regdate=" + regdate + ", inserted_by=" + inserted_by + ", candidate_state_code=" + candidate_state_code
+				+ ", candidacy_date_time=" + candidacy_date_time
 				+ "]";
 	}
 }
